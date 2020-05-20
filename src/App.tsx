f8b2dae6359f8bc6f0db79d61622fc22650a5b62
jhuang429 <jhuang9240@gmail.com>
2020-05-19 22:44:38 -0400

    added betting and money

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,87 @@
-import React, {useState} from 'react';
-import './App.css';
-import Dice from './Components/dice'
-
+import React, { useState } from "react";
+import "./App.css";
+import Dice from "./Components/dice";
 
 function App() {
+  let [currRoll, setCurrRoll] = useState<number[]>([1, 1, 1]);
+  let [history, setHistory] = useState<number[][]>([]);
+  let [bet, setBet] = useState<any>({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 });
+  let [money, setMoney] = useState<number>(100);
+  let [message, setMessage] = useState("")
 
-  let [currRoll, setCurrRoll] = useState<number[]>([1,1,1])
-  let [history, setHistory] = useState<number[][]>([])
+  function rollDice() {
+    let dice1 = Math.floor(Math.random() * 6 + 1);
+    let dice2 = Math.floor(Math.random() * 6 + 1);
+    let dice3 = Math.floor(Math.random() * 6 + 1);
+    setCurrRoll([dice1, dice2, dice3]);
+    setHistory([[dice1, dice2, dice3], ...history]);
 
-  function rollDice(){
-    let dice1 = Math.floor(Math.random() * 6 + 1)
-    let dice2 = Math.floor(Math.random() * 6 + 1)
-    let dice3 = Math.floor(Math.random() * 6 + 1)
-    setCurrRoll([dice1, dice2, dice3])
-    setHistory([...history, [dice1, dice2, dice3]])
+    calcWinnings([dice1, dice2, dice3]);
+  }
 
+  function addbet(num: number) {
+    setBet({ ...bet, [num]: bet[num] + 1 });
+    setMoney(money - 1);
+  }
+
+  function calcWinnings(dices: number[]) {
+    let totalBet = 0;
+    for (let animal in bet) {
+      totalBet += bet[animal];
+    }
+
+    let winnings = 0;
+    let orginalBet: { [key: number]: number } = {};
+
+    for (let die of dices) {
+      winnings += bet[die];
+      orginalBet[die] = bet[die];
+    }
+    let moneyBack = winnings;
+
+    for (let animal in orginalBet) {
+      moneyBack += bet[animal];
+    }
+
+    setMoney(money + moneyBack);
+    setBet({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 });
+    setMessage(winnings? `you won ${winnings}`: `you lost ${totalBet}`)
   }
 
   return (
     <div className="App">
       <div className="Board">
-        <img src="/images/1.png" />
-        <img src="/images/2.png" />
-        <img src="/images/3.png" />
-        <img src="/images/4.png" />
-        <img src="/images/5.png" />
-        <img src="/images/6.png" />
+        <img onClick={() => addbet(1)} src="/images/1.png" />
+        <img onClick={() => addbet(2)} src="/images/2.png" />
+        <img onClick={() => addbet(3)} src="/images/3.png" />
+        <img onClick={() => addbet(4)} src="/images/4.png" />
+        <img onClick={() => addbet(5)} src="/images/5.png" />
+        <img onClick={() => addbet(6)} src="/images/6.png" />
       </div>
-      <div>
-      <button onClick={rollDice}>Roll Dice</button>
+      <div className="console">
         <div className="history">
+          <button onClick={rollDice}>Roll Dice</button>
+          <div></div>
           History:
-          {history.map(arr=><Dice array={arr}/>
-          )}
+          {history.map((arr, index) => (
+            <Dice array={arr} index={index} />
+          ))}
+        </div>
+
+        <div className="bets">
+          bets:
+          <div>Shrimp: {bet[1]}</div>
+          <div>Crab: {bet[2]}</div>
+          <div>Fish: {bet[3]}</div>
+          <div>Stag: {bet[4]}</div>
+          <div>Gourd: {bet[5]}</div>
+          <div>Chicken: {bet[6]}</div>
+        </div>
+        <div className="money">
+        money: <div>{money}</div>
+        last roll: {message}
         </div>
       </div>
-        
     </div>
   );
 }
--- src/Components/dice.tsx
+++ src/Components/dice.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 
-function Dice(props: {array:any}) {
+function Dice(props: {array:any, index:number}) {
   return (
     <div className="set">
-        <img className="dice" src={`/images/${props.array[0]}.png`} />
+        {props.index ?`${props.index}.`: "last"}<img className="dice" src={`/images/${props.array[0]}.png`} />
         <img className="dice"src={`/images/${props.array[1]}.png`} />
         <img className="dice"src={`/images/${props.array[2]}.png`} />
     </div>
